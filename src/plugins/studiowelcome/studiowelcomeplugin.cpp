ea8c7334860e2084d0092e3a74d490b805f3cc0c
Thomas Hartmann <thomas.hartmann@qt.io>
2022-02-07 19:37:16 +0100

    StudioWelcome: Settings for hiding/showing menu entries
    Change-Id: I08bc299e9ce32bde2976dcd901babf91a9a1598c

--- src/plugins/studiowelcome/studiowelcomeplugin.cpp
+++ src/plugins/studiowelcome/studiowelcomeplugin.cpp
@@ -59,17 +59,19 @@
 
 #include <QAbstractListModel>
 #include <QApplication>
+#include <QCheckBox>
 #include <QDesktopServices>
 #include <QFileInfo>
 #include <QFontDatabase>
 #include <QPointer>
-#include <QShortcut>
+#include <QGroupBox>
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQuickItem>
 #include <QQuickView>
 #include <QQuickWidget>
 #include <QSettings>
+#include <QShortcut>
 #include <QTimer>
 
 #include <algorithm>
@@ -735,9 +737,125 @@ WelcomeMode::WelcomeMode()
                                 [](const QString &path) { return QFileInfo::exists(path); }));
 }
 
+static bool hideBuildMenuSetting()
+{
+    return Core::ICore::settings()
+        ->value(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_BUILD, false)
+        .toBool();
+}
+
+static bool hideDebugMenuSetting()
+{
+    return Core::ICore::settings()
+        ->value(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_DEBUG, false)
+        .toBool();
+}
+
+static bool hideToolsMenuSetting()
+{
+    return Core::ICore::settings()->value(Core::Constants::SETTINGS_MENU_HIDE_TOOLS, false).toBool();
+}
+
+static bool hideAnalyzeMenuSetting()
+{
+    return Core::ICore::settings()
+        ->value(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_ANALYZE, false)
+        .toBool();
+}
+
+void setSettingIfDifferent(const QString &key, bool value, bool &dirty)
+{
+    QSettings *s = Core::ICore::settings();
+    if (s->value(key, false).toBool() != value) {
+        dirty = false;
+        s->setValue(key, value);
+    }
+}
+
 WelcomeMode::~WelcomeMode()
 {
     delete m_modeWidget;
+}
+
+StudioSettingsPage::StudioSettingsPage()
+    : m_buildCheckBox(new QCheckBox(tr("Build")))
+    , m_toolsCheckBox(new QCheckBox(tr("Tools")))
+    , m_debugCheckBox(new QCheckBox(tr("Debug")))
+    , m_analyzeCheckBox(new QCheckBox(tr("Analyze")))
+{
+    const QString toolTip = tr(
+        "Hide top-level menus with advanced functionality to simplify the UI. <b>Build</b> is "
+        "generally not required in the context of Qt Design Studio. <b>Tools</b> is only required "
+        "for advanced features like source control. <b>Debug</b> and <b>Analyze</b> are only "
+        "required for debugging and profiling.");
+
+    QVBoxLayout *boxLayout = new QVBoxLayout(this);
+    setLayout(boxLayout);
+    auto groupBox = new QGroupBox(tr("Hide Menu"));
+    groupBox->setToolTip(toolTip);
+    boxLayout->addWidget(groupBox);
+    boxLayout->addSpacerItem(
+        new QSpacerItem(10, 10, QSizePolicy::Expanding, QSizePolicy::Expanding));
+
+    auto verticalLayout = new QVBoxLayout(this);
+    groupBox->setLayout(verticalLayout);
+
+    m_buildCheckBox->setToolTip(toolTip);
+    m_toolsCheckBox->setToolTip(toolTip);
+    m_debugCheckBox->setToolTip(toolTip);
+    m_analyzeCheckBox->setToolTip(toolTip);
+
+    verticalLayout->addWidget(m_buildCheckBox);
+    verticalLayout->addWidget(m_toolsCheckBox);
+    verticalLayout->addWidget(m_debugCheckBox);
+    verticalLayout->addWidget(m_analyzeCheckBox);
+    verticalLayout->addSpacerItem(
+        new QSpacerItem(10, 10, QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+    m_buildCheckBox->setChecked(hideBuildMenuSetting());
+    m_toolsCheckBox->setChecked(hideToolsMenuSetting());
+    m_debugCheckBox->setChecked(hideDebugMenuSetting());
+    m_analyzeCheckBox->setChecked(hideAnalyzeMenuSetting());
+}
+
+void StudioSettingsPage::apply()
+{
+    bool dirty = false;
+
+    setSettingIfDifferent(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_BUILD,
+                          m_buildCheckBox->isChecked(),
+                          dirty);
+
+    setSettingIfDifferent(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_DEBUG,
+                          m_debugCheckBox->isChecked(),
+                          dirty);
+
+    setSettingIfDifferent(ProjectExplorer::Constants::SETTINGS_MENU_HIDE_ANALYZE,
+                          m_analyzeCheckBox->isChecked(),
+                          dirty);
+
+    const bool hideTools = m_toolsCheckBox->isChecked();
+    if (hideTools != hideToolsMenuSetting()) {
+        dirty = true;
+        if (hideTools)
+            Core::ICore::settings()->setValue(Core::Constants::SETTINGS_MENU_MOVE_OPTIONS_TO_EDIT,
+                                              true);
+        Core::ICore::settings()->setValue(Core::Constants::SETTINGS_MENU_HIDE_TOOLS, hideTools);
+    }
+
+    if (dirty) {
+        const QString restartText = tr("The menu visibility change will take effect after restart.");
+        Core::RestartDialog restartDialog(Core::ICore::dialogParent(), restartText);
+        restartDialog.exec();
+    }
+}
+
+StudioWelcomeSettingsPage::StudioWelcomeSettingsPage()
+{
+    setId("Z.StudioWelcome.Settings");
+    setDisplayName(tr("Qt Design Studio Configuration"));
+    setCategory(Core::Constants::SETTINGS_CATEGORY_CORE);
+    setWidgetCreator([] { return new StudioSettingsPage; });
 }
 
 } // namespace Internal
