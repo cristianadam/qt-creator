cristianadam/qt-creator

commit 7ea6492a17736e079ddf192b18d061076dfbf9c7

QmlDesigner: Add imports to subcomponent manager whenever they change
Possible imports were never added to subcomponent manager after
initial design mode activation, and imports added manually via text
editor were also not added to subcomponent manager.

The old implementation also added duplicate imports to subcomponent
manager if imports were removed and readded to the document via
item library.

Fixes: QDS-5345
Change-Id: If45884f50b68282630af1d472af2816640b81c04
Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

src/plugins/qmldesigner/designercore/metainfo/subcomponentmanager.cpp
@@ -69,16 +69,18 @@ SubComponentManager::SubComponentManager(Model *model, QObject *parent)
             this, [this](const QString &path) { parseDirectory(path); });
 }
 
-void SubComponentManager::addImport(const Import &import, int index)
+bool SubComponentManager::addImport(const Import &import, int index)
 {
     if (debug)
         qDebug() << Q_FUNC_INFO << index << import.file().toUtf8();
 
+    bool importExists = false;
     if (import.isFileImport()) {
         QFileInfo dirInfo = QFileInfo(m_filePath.resolved(import.file()).toLocalFile());
         if (dirInfo.exists() && dirInfo.isDir()) {
             const QString canonicalDirPath = dirInfo.canonicalFilePath();
             m_watcher.addPath(canonicalDirPath);
+            importExists = true;
             //m_dirToQualifier.insertMulti(canonicalDirPath, import.qualifier()); ### todo: proper support for import as
         }
     } else {
@@ -91,16 +93,21 @@ void SubComponentManager::addImport(const Import &import, int index)
             if (dirInfo.exists() && dirInfo.isDir()) {
                 const QString canonicalDirPath = dirInfo.canonicalFilePath();
                 m_watcher.addPath(canonicalDirPath);
+                importExists = true;
                 //m_dirToQualifier.insertMulti(canonicalDirPath, import.qualifier()); ### todo: proper support for import as
             }
         }
         // TODO: QDeclarativeDomImport::Library
     }
 
-    if (index == -1)
-        m_imports.append(import);
-    else
-        m_imports.insert(index, import);
+    if (importExists) {
+        if (index == -1)
+            m_imports.append(import);
+        else
+            m_imports.insert(index, import);
+    }
+
+    return importExists;
 }
 
 void SubComponentManager::removeImport(int index)
@@ -544,9 +551,15 @@ void SubComponentManager::update(const QUrl &filePath, const QList<Import> &impo
     parseDirectories();
 }
 
-void SubComponentManager::updateImport(const Import &import)
+void SubComponentManager::addAndParseImport(const Import &import)
 {
-    addImport(import);
+    for (const auto &existingImport : std::as_const(m_imports)) {
+        if (import == existingImport)
+            return;
+    }
+
+    if (!addImport(import))
+        return;
 
     if (import.isFileImport()) {
         QFileInfo dirInfo = QFileInfo(m_filePath.resolved(import.file()).toLocalFile());

src/plugins/qmldesigner/components/edit3d/edit3dview.cpp
@@ -31,7 +31,6 @@
 
 #include <nodeinstanceview.h>
 #include <designeractionmanager.h>
-#include <qmldesignerplugin.h>
 #include <designersettings.h>
 #include <qmldesignerconstants.h>
 #include <viewmanager.h>
@@ -324,10 +323,6 @@ void Edit3DView::addQuick3DImport()
                 } else {
                     model()->changeImports({import}, {});
                 }
-
-                // Subcomponent manager update needed to make item library entries appear
-                QmlDesignerPlugin::instance()->currentDesignDocument()
-                                             ->updateSubcomponentManagerImport(import);
                 return;
             }
         }

src/plugins/qmldesigner/components/integration/designdocument.cpp
@@ -388,9 +388,9 @@ void DesignDocument::updateSubcomponentManager()
                                   currentModel()->imports() + currentModel()->possibleImports());
 }
 
-void DesignDocument::updateSubcomponentManagerImport(const Import &import)
+void DesignDocument::addSubcomponentManagerImport(const Import &import)
 {
-    m_subComponentManager->updateImport(import);
+    m_subComponentManager->addAndParseImport(import);
 }
 
 void DesignDocument::deleteSelected()

src/plugins/qmldesigner/components/integration/designdocument.h
@@ -69,7 +69,7 @@ public:
     void attachRewriterToModel();
     void close();
     void updateSubcomponentManager();
-    void updateSubcomponentManagerImport(const Import &import);
+    void addSubcomponentManagerImport(const Import &import);
 
     bool isUndoAvailable() const;
     bool isRedoAvailable() const;

src/plugins/qmldesigner/components/itemlibrary/itemlibraryassetimporter.cpp
@@ -686,8 +686,6 @@ void ItemLibraryAssetImporter::finalizeQuick3DImport()
 
                                 model->changeImports(newImportsToAdd, {});
                                 transaction.commit();
-                                for (const Import &import : qAsConst(newImportsToAdd))
-                                    doc->updateSubcomponentManagerImport(import);
                             }
                         } catch (const RewritingException &e) {
                             addError(tr("Failed to update imports: %1").arg(e.description()));

src/plugins/qmldesigner/components/itemlibrary/itemlibraryview.cpp
@@ -132,6 +132,10 @@ void ItemLibraryView::modelAboutToBeDetached(Model *model)
 
 void ItemLibraryView::importsChanged(const QList<Import> &addedImports, const QList<Import> &removedImports)
 {
+    DesignDocument *document = QmlDesignerPlugin::instance()->currentDesignDocument();
+    for (const auto &import : addedImports)
+        document->addSubcomponentManagerImport(import);
+
     updateImports();
 
     // TODO: generalize the logic below to allow adding/removing any Qml component when its import is added/removed
@@ -165,6 +169,10 @@ void ItemLibraryView::importsChanged(const QList<Import> &addedImports, const QL
 
 void ItemLibraryView::possibleImportsChanged(const QList<Import> &possibleImports)
 {
+    DesignDocument *document = QmlDesignerPlugin::instance()->currentDesignDocument();
+    for (const auto &import : possibleImports)
+        document->addSubcomponentManagerImport(import);
+
     m_widget->updatePossibleImports(possibleImports);
 }
 

src/plugins/qmldesigner/components/itemlibrary/itemlibrarywidget.cpp
@@ -357,7 +357,6 @@ void ItemLibraryWidget::handleAddImport(int index)
 
     auto document = QmlDesignerPlugin::instance()->currentDesignDocument();
     document->documentModel()->changeImports({import}, {});
-    document->updateSubcomponentManagerImport(import);
 
     m_stackedWidget->setCurrentIndex(0); // switch to the Components view after import is added
     updateSearch();

src/plugins/qmldesigner/components/navigator/navigatortreemodel.cpp
@@ -879,7 +879,6 @@ void NavigatorTreeModel::addImport(const QString &importName)
             if (possImport.url() == import.url()) {
                 import = possImport;
                 m_view->model()->changeImports({import}, {});
-                QmlDesignerPlugin::instance()->currentDesignDocument()->updateSubcomponentManagerImport(import);
                 break;
             }
         }

commit 6f89a89ba5cd9f910077fc8d54722b7e5da55f96

QmlDesigner: Fix loading of .metainfo for versioned folders
This fixes a regression introduced by 7ea6492a17736e079ddf192b18d061076dfbf9c7.
Since we actually check if import folders exist, we did not keep versioned
folder into accoiunt.

I extended and cleaned up the manual test case.

e.g. 'MyPlugin.1'



Task-number: QDS-6078
Change-Id: I3235d8fe498745903c5ca897e25e3d43222c119f

src/plugins/qmldesigner/designercore/metainfo/subcomponentmanager.cpp
@@ -69,10 +69,38 @@ SubComponentManager::SubComponentManager(Model *model, QObject *parent)
             this, [this](const QString &path) { parseDirectory(path); });
 }
 
+QString findFolderForImport(const QStringList &importPaths, const QString &url)
+{
+    if (url.isEmpty())
+        return {};
+
+    QString folderUrl = url;
+    folderUrl.replace('.', '/');
+
+    for (const QString &path : importPaths) {
+        QFileInfo dirInfo = QFileInfo(path + QLatin1Char('/') + folderUrl);
+
+        if (dirInfo.exists() && dirInfo.isDir())
+            return dirInfo.canonicalFilePath();
+
+        const QDir parentDir = dirInfo.dir();
+        if (parentDir.exists()) {
+            const QStringList parts = url.split('.');
+            const QString lastFolder = parts.last();
+
+            const QStringList candidates = parentDir.entryList({lastFolder + ".*"}, QDir::Dirs);
+            if (!candidates.isEmpty())
+                return parentDir.canonicalPath() + "/" + candidates.first();
+        }
+    }
+    return {};
+}
 bool SubComponentManager::addImport(const Import &import, int index)
 {
-    if (debug)
+    if (debug) {
         qDebug() << Q_FUNC_INFO << index << import.file().toUtf8();
+        qDebug() << Q_FUNC_INFO << index << import.url();
+    }
 
     bool importExists = false;
     if (import.isFileImport()) {
@@ -85,19 +113,11 @@ bool SubComponentManager::addImport(const Import &import, int index)
         }
     } else {
         QString url = import.url();
-
-        url.replace(QLatin1Char('.'), QLatin1Char('/'));
-
-        foreach (const QString &path, importPaths()) {
-            QFileInfo dirInfo = QFileInfo(path + QLatin1Char('/') + url);
-            if (dirInfo.exists() && dirInfo.isDir()) {
-                const QString canonicalDirPath = dirInfo.canonicalFilePath();
-                m_watcher.addPath(canonicalDirPath);
-                importExists = true;
-                //m_dirToQualifier.insertMulti(canonicalDirPath, import.qualifier()); ### todo: proper support for import as
-            }
+        const QString result = findFolderForImport(importPaths(), url);
+        if (!result.isEmpty()) {
+            m_watcher.addPath(result);
+            importExists = true;
         }
-        // TODO: QDeclarativeDomImport::Library
     }
 
     if (importExists) {

tests/manual/qml/testprojects/plugins/imports/MyPlugin2.1/MyComponent.qml
@@ -23,21 +23,16 @@
 **
 ****************************************************************************/
 
-import QtQuick
-import MyPlugin 1.0
+import QtQuick 2.0
 
 Rectangle {
-    width: 640
-    height: 480
+    width: 60
+    height: 60
+    color: "green"
+    property alias text: textItem.text
 
-    MyComponent {
-        text: qsTr("Some Text")
-
+    Text {
+        id: textItem
+        anchors.centerIn: parent
     }
 }
-
-/*##^##
-Designer {
-    D{i:0;autoSize:true;height:480;width:640}
-}
-##^##*/

tests/manual/qml/testprojects/plugins/imports/MyPlugin2.1/MyComponent2.qml
@@ -23,21 +23,21 @@
 **
 ****************************************************************************/
 
-import QtQuick
-import MyPlugin 1.0
+import QtQuick 2.0
 
-Rectangle {
-    width: 640
-    height: 480
+Item {
+    property alias text: textItem.text
+    property alias color: rect.color
+    property alias radius: rect.radius
 
-    MyComponent {
-        text: qsTr("Some Text")
+    Rectangle {
+        id: rect
+        anchors.fill: parent
+        color: "green"
 
+        Text {
+            id: textItem
+            anchors.centerIn: parent
+        }
     }
 }
-
-/*##^##
-Designer {
-    D{i:0;autoSize:true;height:480;width:640}
-}
-##^##*/

tests/manual/qml/testprojects/plugins/imports/MyPlugin2.1/designer/MyComponentSpecifics.qml
@@ -1,0 +1,84 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+import QtQuick 2.0
+import HelperWidgets 2.0
+import QtQuick.Layouts 1.0
+
+Column {
+    anchors.left: parent.left
+    anchors.right: parent.right
+
+    Section {
+        anchors.left: parent.left
+        anchors.right: parent.right
+        caption: qsTr("Color")
+
+        ColorEditor {
+            caption: qsTr("Color")
+            backendValue: backendValues.color
+            supportGradient: true
+        }
+
+
+    }
+
+    Section {
+        anchors.left: parent.left
+        anchors.right: parent.right
+        caption: "Rectangle"
+
+        SectionLayout {
+            rows: 2
+
+            Label {
+                text: qsTr("Text")
+            }
+
+            SecondColumnLayout {
+                LineEdit {
+                    backendValue: backendValues.text
+                    Layout.fillWidth: true
+                }
+            }
+
+            Label {
+                text: qsTr("Radius")
+            }
+            SecondColumnLayout {
+                SpinBox {
+                    backendValue: backendValues.radius
+                    hasSlider: true
+                    Layout.preferredWidth: 80
+                    minimumValue: 0
+                    maximumValue: Math.min(backendValues.height.value, backendValues.width.value) / 2
+                }
+                ExpandingSpacer {
+
+                }
+            }
+        }
+    }
+}

tests/manual/qml/testprojects/plugins/imports/MyPlugin2.1/designer/MyPlugin.metainfo
@@ -1,0 +1,44 @@
+MetaInfo {
+    Type {
+        name: "MyPlugin2.MyComponent"
+        icon: ":/qtquickplugin/images/item-icon16.png"
+
+        ItemLibraryEntry {
+            name: "My Component"
+            category: "My Test Plugin 2"
+            libraryIcon: ":/qtquickplugin/images/item-icon.png"
+            version: "1.0"
+            requiredImport: "MyPlugin2"
+
+            Property {
+                name: "text"
+                type: "binding"
+                value: "qsTr(\"This is text\")"
+            }
+        }
+    }
+
+    Type {
+        name: "MyPlugin2.MyComponent2"
+        icon: ":/qtquickplugin/images/item-icon16.png"
+
+        ItemLibraryEntry {
+            name: "My Component 2"
+            category: "My Test Plugin 2"
+            libraryIcon: ":/qtquickplugin/images/item-icon.png"
+            version: "1.0"
+            requiredImport: "MyPlugin2"
+
+            Property {
+                name: "text"
+                type: "binding"
+                value: "qsTr(\"This is text\")"
+            }
+
+            Property { name: "width"; type: "int"; value: 200; }
+            Property { name: "height"; type: "int"; value: 200; }
+            Property { name: "color"; type: "QColor"; value: "red"; }
+        }
+    }
+
+}

tests/manual/qml/testprojects/plugins/imports/MyPlugin2.1/qmldir
@@ -1,0 +1,2 @@
+MyComponent 1.0 MyComponent.qml
+MyComponent2 1.0 MyComponent2.qml

tests/manual/qml/testprojects/plugins/plugins2.qml
@@ -23,10 +23,11 @@
 **
 ****************************************************************************/
 
-import QtQuick
-import MyPlugin 1.0
+import QtQuick 2.3
+import MyPlugin2 1.0
 
 Rectangle {
+
     width: 640
     height: 480
 
@@ -35,9 +36,3 @@ Rectangle {
 
     }
 }
-
-/*##^##
-Designer {
-    D{i:0;autoSize:true;height:480;width:640}
-}
-##^##*/