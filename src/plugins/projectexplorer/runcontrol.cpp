0a58f95ce23571cd305154dd96b5337408746fb6
hjk <hjk@qt.io>
2022-05-25 13:53:21 +0200

    ProjectExplorer: Replace somet uses of Runnable::device
    Change-Id: I76ccbe9a56b9ecdc07fc85885b34f2a31c69fc07

--- src/plugins/projectexplorer/runcontrol.cpp
+++ src/plugins/projectexplorer/runcontrol.cpp
@@ -1398,7 +1398,7 @@ void SimpleTargetRunnerPrivate::handleStandardError()
 
 void SimpleTargetRunnerPrivate::start()
 {
-    m_isLocal = m_runnable.device.isNull() || m_runnable.device.dynamicCast<const DesktopDevice>();
+    m_isLocal = !m_runnable.command.executable().needsDevice();
 
     m_resultData = {};
 
@@ -1500,8 +1500,7 @@ void SimpleTargetRunnerPrivate::forwardDone()
 
 void SimpleTargetRunnerPrivate::forwardStarted()
 {
-    const bool isDesktop = m_runnable.device.isNull()
-                        || m_runnable.device.dynamicCast<const DesktopDevice>();
+    const bool isDesktop = !m_runnable.command.executable().needsDevice();
     if (isDesktop) {
         // Console processes only know their pid after being started
         ProcessHandle pid{privateApplicationPID()};
@@ -1535,10 +1534,9 @@ void SimpleTargetRunner::start()
     d->m_runAsRoot = runAsRoot;
 
     const QString msg = RunControl::tr("Starting %1...").arg(d->m_runnable.command.toUserOutput());
-    appendMessage(msg, Utils::NormalMessageFormat);
+    appendMessage(msg, NormalMessageFormat);
 
-    const bool isDesktop = d->m_runnable.device.isNull()
-                        || d->m_runnable.device.dynamicCast<const DesktopDevice>();
+    const bool isDesktop = !d->m_runnable.command.executable().needsDevice();
     if (isDesktop && d->m_runnable.command.isEmpty()) {
         reportFailure(RunControl::tr("No executable specified."));
         return;
