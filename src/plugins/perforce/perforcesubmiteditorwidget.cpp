128cb210bc3422b2a512ba215ed4748ed81e7384
hjk <hjk@qt.io>
2022-10-04 13:51:26 +0200

    Perforce: Inline submitpanel.ui
    Some unusual stuff removed, some unusual stuff left in.
    
    Change-Id: I53d9f4677540c2401d6ddfcd687b5f18c5f70667

--- src/plugins/perforce/perforcesubmiteditorwidget.cpp
+++ src/plugins/perforce/perforcesubmiteditorwidget.cpp
@@ -3,13 +3,54 @@
 
 #include "perforcesubmiteditorwidget.h"
 
-namespace Perforce {
-namespace Internal {
+#include <utils/layoutbuilder.h>
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+
+using namespace Utils;
+
+namespace Perforce::Internal {
+
+class SubmitPanel : public QGroupBox
+{
+public:
+    SubmitPanel()
+    {
+        resize(402, 134);
+        setFlat(true);
+        setTitle(tr("Submit"));
+
+        m_changeNumber = new QLabel(this);
+        m_changeNumber->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByMouse);
+
+        m_clientName = new QLabel(this);
+        m_clientName->setTextInteractionFlags(Qt::LinksAccessibleByMouse);
+
+        m_userName = new QLabel(this);
+        m_userName->setTextInteractionFlags(Qt::LinksAccessibleByMouse);
+
+        using namespace Layouting;
+
+        Form {
+            tr("Change:"), m_changeNumber, br,
+            tr("Client:"), m_clientName, br,
+            tr("User:"), m_userName
+        }.attachTo(this, WithoutMargins);
+    }
+
+    QLabel *m_changeNumber;
+    QLabel *m_clientName;
+    QLabel *m_userName;
+};
 
 PerforceSubmitEditorWidget::PerforceSubmitEditorWidget() :
-    m_submitPanel(new QGroupBox)
+    m_submitPanel(new SubmitPanel)
 {
-    m_submitPanelUi.setupUi(m_submitPanel);
     insertTopWidget(m_submitPanel);
 }
 
@@ -17,10 +58,9 @@ void PerforceSubmitEditorWidget::setData(const QString &change,
                                          const QString &client,
                                          const QString &userName)
 {
-    m_submitPanelUi.changeNumber->setText(change);
-    m_submitPanelUi.clientName->setText(client);
-    m_submitPanelUi.userName->setText(userName);
+    m_submitPanel->m_changeNumber->setText(change);
+    m_submitPanel->m_clientName->setText(client);
+    m_submitPanel->m_userName->setText(userName);
 }
 
-} // namespace Internal
-} // namespace Perforce
+} // Perforce::Internal
