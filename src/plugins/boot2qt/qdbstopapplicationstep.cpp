4ac3b3a7f7469a3104a7835d5abce0090c27d996
hjk <hjk@qt.io>
2022-10-13 15:17:20 +0200

    WIP: Merge abstractremotelinuxdeploystep with its service file pairs
    Change-Id: I720a8c58c684c05971723351a3e4155669dc8440

--- src/plugins/boot2qt/qdbstopapplicationstep.cpp
+++ src/plugins/boot2qt/qdbstopapplicationstep.cpp
@@ -11,7 +11,6 @@
 #include <projectexplorer/runcontrol.h>
 #include <projectexplorer/target.h>
 
-#include <remotelinux/abstractremotelinuxdeployservice.h>
 #include <remotelinux/abstractremotelinuxdeploystep.h>
 
 #include <utils/qtcprocess.h>
--- src/plugins/remotelinux/abstractremotelinuxdeploystep.cpp
+++ src/plugins/remotelinux/abstractremotelinuxdeploystep.cpp
@@ -3,15 +3,171 @@
 
 #include "abstractremotelinuxdeploystep.h"
 
-#include "abstractremotelinuxdeployservice.h"
+#include "deploymenttimeinfo.h"
+#include "remotelinux_export.h"
 #include "remotelinuxtr.h"
 
-#include <projectexplorer/projectexplorerconstants.h>
+#include <projectexplorer/deployablefile.h>
+#include <projectexplorer/devicesupport/idevice.h>
+#include <projectexplorer/devicesupport/idevicefwd.h>
 #include <projectexplorer/kitinformation.h>
+#include <projectexplorer/kitinformation.h>
+#include <projectexplorer/projectexplorerconstants.h>
+#include <projectexplorer/target.h>
+
+#include <utils/qtcassert.h>
+
+#include <QtCore/qcontainerfwd.h>
+#include <QDateTime>
+#include <QFileInfo>
+#include <QPointer>
 
 using namespace ProjectExplorer;
 
+namespace ProjectExplorer {
+class DeployableFile;
+class Kit;
+class Target;
+}
+
 namespace RemoteLinux {
+namespace Internal {
+
+namespace {
+enum State { Inactive, Deploying };
+} // anonymous namespace
+
+class AbstractRemoteLinuxDeployServicePrivate
+{
+public:
+    IDevice::ConstPtr deviceConfiguration;
+    QPointer<Target> target;
+
+    DeploymentTimeInfo deployTimes;
+    State state = Inactive;
+    bool stopRequested = false;
+};
+} // namespace Internal
+
+using namespace Internal;
+
+AbstractRemoteLinuxDeployService::AbstractRemoteLinuxDeployService(QObject *parent)
+    : QObject(parent), d(new AbstractRemoteLinuxDeployServicePrivate)
+{
+}
+
+AbstractRemoteLinuxDeployService::~AbstractRemoteLinuxDeployService()
+{
+    delete d;
+}
+
+const Target *AbstractRemoteLinuxDeployService::target() const
+{
+    return d->target;
+}
+
+const Kit *AbstractRemoteLinuxDeployService::kit() const
+{
+    return d->target ? d->target->kit() : nullptr;
+}
+
+IDevice::ConstPtr AbstractRemoteLinuxDeployService::deviceConfiguration() const
+{
+    return d->deviceConfiguration;
+}
+
+void AbstractRemoteLinuxDeployService::saveDeploymentTimeStamp(const DeployableFile &deployableFile,
+                                                               const QDateTime &remoteTimestamp)
+{
+    d->deployTimes.saveDeploymentTimeStamp(deployableFile, kit(), remoteTimestamp);
+}
+
+bool AbstractRemoteLinuxDeployService::hasLocalFileChanged(
+        const DeployableFile &deployableFile) const
+{
+    return d->deployTimes.hasLocalFileChanged(deployableFile, kit());
+}
+
+bool AbstractRemoteLinuxDeployService::hasRemoteFileChanged(
+        const DeployableFile &deployableFile, const QDateTime &remoteTimestamp) const
+{
+    return d->deployTimes.hasRemoteFileChanged(deployableFile, kit(), remoteTimestamp);
+}
+
+void AbstractRemoteLinuxDeployService::setTarget(Target *target)
+{
+    d->target = target;
+    d->deviceConfiguration = DeviceKitAspect::device(kit());
+}
+
+void AbstractRemoteLinuxDeployService::setDevice(const IDevice::ConstPtr &device)
+{
+    d->deviceConfiguration = device;
+}
+
+void AbstractRemoteLinuxDeployService::start()
+{
+    QTC_ASSERT(d->state == Inactive, return);
+
+    const CheckResult check = isDeploymentPossible();
+    if (!check) {
+        emit errorMessage(check.errorMessage());
+        emit finished();
+        return;
+    }
+
+    if (!isDeploymentNecessary()) {
+        emit progressMessage(Tr::tr("No deployment action necessary. Skipping."));
+        emit finished();
+        return;
+    }
+
+    d->state = Deploying;
+    doDeploy();
+}
+
+void AbstractRemoteLinuxDeployService::stop()
+{
+    if (d->stopRequested)
+        return;
+
+    if (d->state == Deploying) {
+        d->stopRequested = true;
+        stopDeployment();
+    }
+}
+
+CheckResult AbstractRemoteLinuxDeployService::isDeploymentPossible() const
+{
+    if (!deviceConfiguration())
+        return CheckResult::failure(Tr::tr("No device configuration set."));
+    return CheckResult::success();
+}
+
+QVariantMap AbstractRemoteLinuxDeployService::exportDeployTimes() const
+{
+    return d->deployTimes.exportDeployTimes();
+}
+
+void AbstractRemoteLinuxDeployService::importDeployTimes(const QVariantMap &map)
+{
+    d->deployTimes.importDeployTimes(map);
+}
+
+void AbstractRemoteLinuxDeployService::handleDeploymentDone()
+{
+    QTC_ASSERT(d->state == Deploying, return);
+
+    setFinished();
+}
+
+void AbstractRemoteLinuxDeployService::setFinished()
+{
+    d->state = Inactive;
+    d->stopRequested = false;
+    emit finished();
+}
+
 namespace Internal {
 
 class AbstractRemoteLinuxDeployStepPrivate
