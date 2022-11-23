0422761a8686fd595cfc31f6197c57b382e6defb
Christian Stenger <christian.stenger@qt.io>
2022-11-23 16:14:31 +0100

    Squish: Extract server into separate class
    Change-Id: Iff370cd3571598ee32774e02f4f883d07e4c4685

---
+++ src/plugins/squish/squishprocessbase.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0 WITH Qt-GPL-exception-1.0
+
+#include "squishprocessbase.h"
+
+namespace Squish::Internal {
+
+SquishProcessBase::SquishProcessBase(QObject *parent)
+    : QObject(parent)
+{
+    connect(&m_process, &Utils::QtcProcess::readyReadStandardError,
+            this, &SquishProcessBase::onErrorOutput);
+    connect(&m_process, &Utils::QtcProcess::done,
+            this, &SquishProcessBase::onDone);
+}
+
+} // namespace Squish::Internal
---
+++ src/plugins/squish/squishprocessbase.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0 WITH Qt-GPL-exception-1.0
+
+#pragma once
+
+#include <utils/qtcprocess.h>
+
+#include <QObject>
+
+namespace Squish::Internal {
+
+enum SquishProcessState { Idle, Starting, Started, StartFailed, Stopped, StopFailed };
+
+class SquishProcessBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SquishProcessBase(QObject *parent = nullptr);
+    ~SquishProcessBase() = default;
+    SquishProcessState processState() const { return m_state; }
+
+    inline bool isRunning() const { return m_process.isRunning(); }
+    inline Utils::ProcessResult result() const { return m_process.result(); }
+    inline QProcess::ProcessError error() const { return m_process.error(); }
+    inline QProcess::ProcessState state() const { return m_process.state(); }
+
+    void closeProcess() { m_process.close(); }
+
+signals:
+    void logOutputReceived(const QString &output);
+    void stateChanged(SquishProcessState state);
+
+protected:
+    virtual void onDone() {}
+    virtual void onErrorOutput() {}
+
+    Utils::QtcProcess m_process;
+    SquishProcessState m_state = Idle;
+};
+
+} // namespace Squish::Internal
