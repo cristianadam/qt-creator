cristianadam/qt-creator

commit d05c5b7d07194b7c0fe938e88e63cfaab9ee5f4d

AutoTest: Use TestResult as value type
Don't construct it on heap and don't use shared pointer for it.

Change-Id: I51c9da405ed14d24b5f20242b4d049f9e2958f09
Reviewed-by: Christian Stenger <christian.stenger@qt.io>
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

src/plugins/autotest/gtest/gtestoutputreader.cpp
@@ -15,7 +15,7 @@
 namespace Autotest {
 namespace Internal {
 
-GTestOutputReader::GTestOutputReader(const QFutureInterface<TestResultPtr> &futureInterface,
+GTestOutputReader::GTestOutputReader(const QFutureInterface<TestResult> &futureInterface,
                                      Utils::QtcProcess *testApplication,
                                      const Utils::FilePath &buildDirectory,
                                      const Utils::FilePath &projectFile)
@@ -70,9 +70,9 @@ void GTestOutputReader::processOutputLine(const QByteArray &outputLine)
             m_description = line;
             if (m_iteration > 1)
                 m_description.append(' ' + Tr::tr("(iteration %1)").arg(m_iteration));
-            TestResultPtr testResult = TestResultPtr(new GTestResult(id(), {}, m_projectFile));
-            testResult->setResult(ResultType::MessageInternal);
-            testResult->setDescription(m_description);
+            GTestResult testResult(id(), {}, m_projectFile);
+            testResult.setResult(ResultType::MessageInternal);
+            testResult.setDescription(m_description);
             reportResult(testResult);
             m_description.clear();
         } else if (ExactMatch match = disabledTests.match(line)) {
@@ -83,66 +83,66 @@ void GTestOutputReader::processOutputLine(const QByteArray &outputLine)
     }
 
     if (ExactMatch match = testEnds.match(line)) {
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(ResultType::TestEnd);
-        testResult->setDescription(Tr::tr("Test execution took %1").arg(match.captured(2)));
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(ResultType::TestEnd);
+        testResult.setDescription(Tr::tr("Test execution took %1").arg(match.captured(2)));
         reportResult(testResult);
         m_currentTestSuite.clear();
         m_currentTestCase.clear();
     } else if (ExactMatch match = newTestStarts.match(line)) {
         setCurrentTestSuite(match.captured(1));
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(ResultType::TestStart);
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(ResultType::TestStart);
         if (m_iteration > 1) {
-            testResult->setDescription(Tr::tr("Repeating test suite %1 (iteration %2)")
+            testResult.setDescription(Tr::tr("Repeating test suite %1 (iteration %2)")
                                        .arg(m_currentTestSuite).arg(m_iteration));
         } else {
-            testResult->setDescription(Tr::tr("Executing test suite %1").arg(m_currentTestSuite));
+            testResult.setDescription(Tr::tr("Executing test suite %1").arg(m_currentTestSuite));
         }
         reportResult(testResult);
     } else if (ExactMatch match = newTestSetStarts.match(line)) {
         m_testSetStarted = true;
         setCurrentTestCase(match.captured(1));
-        TestResultPtr testResult = TestResultPtr(new GTestResult({}, {}, m_projectFile));
-        testResult->setResult(ResultType::MessageCurrentTest);
-        testResult->setDescription(Tr::tr("Entering test case %1").arg(m_currentTestCase));
+        GTestResult testResult("internal", {}, m_projectFile);
+        testResult.setResult(ResultType::MessageCurrentTest);
+        testResult.setDescription(Tr::tr("Entering test case %1").arg(m_currentTestCase));
         reportResult(testResult);
         m_description.clear();
     } else if (ExactMatch match = testSetSuccess.match(line)) {
         m_testSetStarted = false;
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(ResultType::Pass);
-        testResult->setDescription(m_description);
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(ResultType::Pass);
+        testResult.setDescription(m_description);
         reportResult(testResult);
         m_description.clear();
         testResult = createDefaultResult();
-        testResult->setResult(ResultType::MessageInternal);
-        testResult->setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
+        testResult.setResult(ResultType::MessageInternal);
+        testResult.setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
         reportResult(testResult);
         m_futureInterface.setProgressValue(m_futureInterface.progressValue() + 1);
     } else if (ExactMatch match = testSetFail.match(line)) {
         m_testSetStarted = false;
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(ResultType::Fail);
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(ResultType::Fail);
         m_description.chop(1);
         handleDescriptionAndReportResult(testResult);
         testResult = createDefaultResult();
-        testResult->setResult(ResultType::MessageInternal);
-        testResult->setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
+        testResult.setResult(ResultType::MessageInternal);
+        testResult.setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
         reportResult(testResult);
         m_futureInterface.setProgressValue(m_futureInterface.progressValue() + 1);
     } else if (ExactMatch match = testSetSkipped.match(line)) {
         if (!m_testSetStarted)  // ignore SKIPPED at summary
             return;
         m_testSetStarted = false;
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(ResultType::Skip);
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(ResultType::Skip);
         m_description.chop(1);
         m_description.prepend(match.captured(1) + '\n');
         handleDescriptionAndReportResult(testResult);
         testResult = createDefaultResult();
-        testResult->setResult(ResultType::MessageInternal);
-        testResult->setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
+        testResult.setResult(ResultType::MessageInternal);
+        testResult.setDescription(Tr::tr("Execution took %1.").arg(match.captured(2)));
         reportResult(testResult);
     } else if (ExactMatch match = logging.match(line)) {
         const QString severity = match.captured(1).trimmed();
@@ -153,13 +153,13 @@ void GTestOutputReader::processOutputLine(const QByteArray &outputLine)
         case 'E': type = ResultType::MessageError; break;   // ERROR
         case 'F': type = ResultType::MessageFatal; break;   // FATAL
         }
-        TestResultPtr testResult = createDefaultResult();
-        testResult->setResult(type);
-        testResult->setLine(match.captured(3).toInt());
+        TestResult testResult = createDefaultResult();
+        testResult.setResult(type);
+        testResult.setLine(match.captured(3).toInt());
         const Utils::FilePath file = constructSourceFilePath(m_buildDir, match.captured(2));
         if (file.exists())
-            testResult->setFileName(file);
-        testResult->setDescription(match.captured(4));
+            testResult.setFileName(file);
+        testResult.setDescription(match.captured(4));
         reportResult(testResult);
     } else if (ExactMatch match = testDeath.match(line)) {
         m_description.append(line);
@@ -175,19 +175,15 @@ void GTestOutputReader::processStdError(const QByteArray &outputLine)
     emit newOutputLineAvailable(outputLine, OutputChannel::StdErr);
 }
 
-TestResultPtr GTestOutputReader::createDefaultResult() const
+TestResult GTestOutputReader::createDefaultResult() const
 {
-    GTestResult *result = new GTestResult(id(), m_currentTestSuite, m_projectFile,
-                                          m_currentTestCase, m_iteration);
-
-    const ITestTreeItem *testItem = result->findTestTreeItem();
-
+    GTestResult result(id(), m_currentTestSuite, m_projectFile, m_currentTestCase, m_iteration);
+    const ITestTreeItem *testItem = result.findTestTreeItem();
     if (testItem && testItem->line()) {
-        result->setFileName(testItem->filePath());
-        result->setLine(testItem->line());
+        result.setFileName(testItem->filePath());
+        result.setLine(testItem->line());
     }
-
-    return TestResultPtr(result);
+    return result;
 }
 
 void GTestOutputReader::setCurrentTestCase(const QString &testCase)
@@ -200,13 +196,13 @@ void GTestOutputReader::setCurrentTestSuite(const QString &testSuite)
     m_currentTestSuite = testSuite;
 }
 
-void GTestOutputReader::handleDescriptionAndReportResult(TestResultPtr testResult)
+void GTestOutputReader::handleDescriptionAndReportResult(const TestResult &testResult)
 {
     static const QRegularExpression failureLocation("^(.*):(\\d+): Failure$");
     static const QRegularExpression skipOrErrorLocation("^(.*)\\((\\d+)\\): (Skipped|error:.*)$");
 
     QStringList resultDescription;
-
+    TestResult result = testResult;
     for (const QString &output : m_description.split('\n')) {
         QRegularExpressionMatch innerMatch = failureLocation.match(output);
         if (!innerMatch.hasMatch()) {
@@ -216,20 +212,20 @@ void GTestOutputReader::handleDescriptionAndReportResult(TestResultPtr testResul
                 continue;
              }
         }
-        testResult->setDescription(resultDescription.join('\n'));
+        result.setDescription(resultDescription.join('\n'));
         reportResult(testResult);
         resultDescription.clear();
 
-        testResult = createDefaultResult();
-        testResult->setResult(ResultType::MessageLocation);
-        testResult->setLine(innerMatch.captured(2).toInt());
+        result = createDefaultResult();
+        result.setResult(ResultType::MessageLocation);
+        result.setLine(innerMatch.captured(2).toInt());
         const Utils::FilePath file = constructSourceFilePath(m_buildDir, innerMatch.captured(1));
         if (file.exists())
-            testResult->setFileName(file);
+            result.setFileName(file);
         resultDescription << output;
     }
-    testResult->setDescription(resultDescription.join('\n'));
-    reportResult(testResult);
+    result.setDescription(resultDescription.join('\n'));
+    reportResult(result);
     m_description.clear();
 }
 

src/plugins/autotest/qtest/qttestoutputreader.cpp
@@ -103,7 +103,7 @@ static QString constructBenchmarkInformation(const QString &metric, double value
             .arg(iterations);
 }
 
-QtTestOutputReader::QtTestOutputReader(const QFutureInterface<TestResultPtr> &futureInterface,
+QtTestOutputReader::QtTestOutputReader(const QFutureInterface<TestResult> &futureInterface,
                                        Utils::QtcProcess *testApplication,
                                        const Utils::FilePath &buildDirectory,
                                        const Utils::FilePath &projectFile,
@@ -130,11 +130,9 @@ void QtTestOutputReader::processOutputLine(const QByteArray &outputLine)
     }
 }
 
-TestResultPtr QtTestOutputReader::createDefaultResult() const
+TestResult QtTestOutputReader::createDefaultResult() const
 {
-    QtTestResult *result = new QtTestResult(id(), m_className, m_projectFile, m_testType,
-                                            m_testCase, m_dataTag);
-    return TestResultPtr(result);
+    return QtTestResult(id(), m_className, m_projectFile, m_testType, m_testCase, m_dataTag);
 }
 
 static QString trQtVersion(const QString &version)
@@ -455,73 +453,75 @@ void QtTestOutputReader::processSummaryFinishOutput()
 
 void QtTestOutputReader::sendCompleteInformation()
 {
-    TestResultPtr testResult = createDefaultResult();
-    testResult->setResult(m_result);
+    TestResult testResult = createDefaultResult();
+    testResult.setResult(m_result);
 
     if (m_lineNumber) {
-        testResult->setFileName(m_file);
-        testResult->setLine(m_lineNumber);
+        testResult.setFileName(m_file);
+        testResult.setLine(m_lineNumber);
     } else {
-        const ITestTreeItem *testItem = testResult->findTestTreeItem();
+        const ITestTreeItem *testItem = testResult.findTestTreeItem();
         if (testItem && testItem->line()) {
-            testResult->setFileName(testItem->filePath());
-            testResult->setLine(testItem->line());
+            testResult.setFileName(testItem->filePath());
+            testResult.setLine(testItem->line());
         }
     }
-    testResult->setDescription(m_description);
+    testResult.setDescription(m_description);
     reportResult(testResult);
 }
 
 void QtTestOutputReader::sendMessageCurrentTest()
 {
-    QtTestResult *testResult = new QtTestResult({}, {}, m_projectFile, m_testType);
-    testResult->setResult(ResultType::MessageCurrentTest);
-    testResult->setDescription(Tr::tr("Entering test function %1::%2").arg(m_className, m_testCase));
-    reportResult(TestResultPtr(testResult));
+    QtTestResult result("internal", {}, m_projectFile, m_testType);
+    result.setResult(ResultType::MessageCurrentTest);
+    result.setDescription(Tr::tr("Entering test function %1::%2").arg(m_className, m_testCase));
+    reportResult(result);
 }
 
 void QtTestOutputReader::sendStartMessage(bool isFunction)
 {
-    TestResultPtr testResult = createDefaultResult();
-    testResult->setResult(ResultType::TestStart);
-    testResult->setDescription(isFunction ? Tr::tr("Executing test function %1").arg(m_testCase)
-                                          : Tr::tr("Executing test case %1").arg(m_className));
-    const ITestTreeItem *testItem = testResult->findTestTreeItem();
+    TestResult result = createDefaultResult();
+    result.setResult(ResultType::TestStart);
+    result.setDescription(isFunction ? Tr::tr("Executing test function %1").arg(m_testCase)
+                                     : Tr::tr("Executing test case %1").arg(m_className));
+    const ITestTreeItem *testItem = result.findTestTreeItem();
     if (testItem && testItem->line()) {
-        testResult->setFileName(testItem->filePath());
-        testResult->setLine(testItem->line());
+        result.setFileName(testItem->filePath());
+        result.setLine(testItem->line());
     }
-    reportResult(testResult);
+    reportResult(result);
 }
 
 void QtTestOutputReader::sendFinishMessage(bool isFunction)
 {
-    TestResultPtr testResult = createDefaultResult();
-    testResult->setResult(ResultType::TestEnd);
+    TestResult result = createDefaultResult();
+    result.setResult(ResultType::TestEnd);
     if (!m_duration.isEmpty()) {
-        testResult->setDescription(isFunction ? Tr::tr("Execution took %1 ms.").arg(m_duration)
-                                              : Tr::tr("Test execution took %1 ms.").arg(m_duration));
+        result.setDescription(isFunction ? Tr::tr("Execution took %1 ms.").arg(m_duration)
+                                         : Tr::tr("Test execution took %1 ms.").arg(m_duration));
     } else {
-        testResult->setDescription(isFunction ? Tr::tr("Test function finished.")
-                                              : Tr::tr("Test finished."));
+        result.setDescription(isFunction ? Tr::tr("Test function finished.")
+                                         : Tr::tr("Test finished."));
     }
-    reportResult(testResult);
+    reportResult(result);
 }
 
 void QtTestOutputReader::handleAndSendConfigMessage(const QRegularExpressionMatch &config)
 {
-    TestResultPtr testResult = createDefaultResult();
-    testResult->setResult(ResultType::MessageInternal);
-    testResult->setDescription(trQtVersion(config.captured(3)));
-    reportResult(testResult);
-    testResult = createDefaultResult();
-    testResult->setResult(ResultType::MessageInternal);
-    testResult->setDescription(trQtBuild(config.captured(2)));
-    reportResult(testResult);
-    testResult = createDefaultResult();
-    testResult->setResult(ResultType::MessageInternal);
-    testResult->setDescription(trQtestVersion(config.captured(1)));
-    reportResult(testResult);
+    TestResult result = createDefaultResult();
+    result.setResult(ResultType::MessageInternal);
+    result.setDescription(trQtVersion(config.captured(3)));
+    reportResult(result);
+
+    result = createDefaultResult();
+    result.setResult(ResultType::MessageInternal);
+    result.setDescription(trQtBuild(config.captured(2)));
+    reportResult(result);
+
+    result = createDefaultResult();
+    result.setResult(ResultType::MessageInternal);
+    result.setDescription(trQtestVersion(config.captured(1)));
+    reportResult(result);
 }
 
 } // namespace Internal

commit 3b37269e396bb7f5d12f0882b87bded9208085ea

AutoTest: Fix handling of test results
Broke with d05c5b7d07194b7c0fe938e88e63cfaab9ee5f4d. The id is
used to identify the application running a test or a global
message like some warnings or the information regarding the
current running test.
Fixes its usage and its display inside the results pane.

Change-Id: I9949aec3fc2b7354de149433b7127933f2d9bf21

src/plugins/autotest/gtest/gtestoutputreader.cpp
@@ -105,7 +105,7 @@ void GTestOutputReader::processOutputLine(const QByteArray &outputLine)
     } else if (ExactMatch match = newTestSetStarts.match(line)) {
         m_testSetStarted = true;
         setCurrentTestCase(match.captured(1));
-        GTestResult testResult("internal", {}, m_projectFile);
+        GTestResult testResult(std::nullopt, {}, m_projectFile);
         testResult.setResult(ResultType::MessageCurrentTest);
         testResult.setDescription(Tr::tr("Entering test case %1").arg(m_currentTestCase));
         reportResult(testResult);

src/plugins/autotest/gtest/gtestresult.cpp
@@ -123,7 +123,8 @@ static ResultHooks::DirectParentHook directParentHook(const QString &testCaseNam
     };
 }
 
-GTestResult::GTestResult(const QString &id, const QString &name, const FilePath &projectFile,
+GTestResult::GTestResult(const std::optional<QString> &id, const QString &name,
+                         const FilePath &projectFile,
                          const QString &testCaseName, int iteration)
     : TestResult(id, name, {QVariant::fromValue(GTestData{testCaseName, iteration}),
                             outputStringHook(testCaseName),

src/plugins/autotest/gtest/gtestresult.h
@@ -14,7 +14,8 @@ namespace Internal {
 class GTestResult : public TestResult
 {
 public:
-    GTestResult(const QString &id, const QString &name, const Utils::FilePath &projectFile,
+    GTestResult(const std::optional<QString> &id, const QString &name,
+                const Utils::FilePath &projectFile,
                 const QString &testCaseName = {}, int iteration = 1);
 };
 

src/plugins/autotest/qtest/qttestoutputreader.cpp
@@ -474,7 +474,7 @@ void QtTestOutputReader::sendCompleteInformation()
 
 void QtTestOutputReader::sendMessageCurrentTest()
 {
-    QtTestResult result("internal", {}, m_projectFile, m_testType);
+    QtTestResult result(std::nullopt, {}, m_projectFile, m_testType);
     result.setResult(ResultType::MessageCurrentTest);
     result.setDescription(Tr::tr("Entering test function %1::%2").arg(m_className, m_testCase));
     reportResult(result);

src/plugins/autotest/qtest/qttestresult.cpp
@@ -215,7 +215,8 @@ static ResultHooks::CreateResultHook createResultHook(const FilePath &projectFil
     };
 }
 
-QtTestResult::QtTestResult(const QString &id, const QString &name, const FilePath &projectFile,
+QtTestResult::QtTestResult(const std::optional<QString> &id,
+                           const QString &name, const FilePath &projectFile,
                            TestType type, const QString &functionName, const QString &dataTag)
     : TestResult(id, name, {QVariant::fromValue(QtTestData{projectFile, type, functionName, dataTag}),
                             outputStringHook(functionName, dataTag),

src/plugins/autotest/qtest/qttestresult.h
@@ -15,7 +15,8 @@ namespace Internal {
 class QtTestResult : public TestResult
 {
 public:
-    QtTestResult(const QString &id, const QString &name, const Utils::FilePath &projectFile,
+    QtTestResult(const std::optional<QString> &id, const QString &name,
+                 const Utils::FilePath &projectFile,
                  TestType type, const QString &functionName = {}, const QString &dataTag = {});
 };
 

src/plugins/autotest/testresult.cpp
@@ -10,7 +10,8 @@ using namespace Utils;
 
 namespace Autotest {
 
-TestResult::TestResult(const QString &id, const QString &name, const ResultHooks &hooks)
+TestResult::TestResult(const std::optional<QString> &id, const QString &name,
+                       const ResultHooks &hooks)
     : m_id(id)
     , m_name(name)
     , m_hooks(hooks)
@@ -19,7 +20,7 @@ TestResult::TestResult(const QString &id, const QString &name, const ResultHooks
 
 bool TestResult::isValid() const
 {
-    return !m_id.isEmpty();
+    return m_id.has_value();
 }
 
 const QString TestResult::outputString(bool selected) const
@@ -28,7 +29,7 @@ const QString TestResult::outputString(bool selected) const
         return m_hooks.outputString(*this, selected);
 
     if (m_result == ResultType::Application)
-        return m_id;
+        return id();
     return selected ? m_description : m_description.split('\n').first();
 }
 
@@ -166,7 +167,7 @@ QColor TestResult::colorForType(const ResultType type)
 bool TestResult::isDirectParentOf(const TestResult &other, bool *needsIntermediate) const
 {
     QTC_ASSERT(other.isValid(), return false);
-    const bool ret = !m_id.isEmpty() && m_id == other.m_id && m_name == other.m_name;
+    const bool ret = m_id && m_id == other.m_id && m_name == other.m_name;
     if (!ret)
         return false;
     if (m_hooks.directParent)
@@ -179,7 +180,7 @@ bool TestResult::isIntermediateFor(const TestResult &other) const
     QTC_ASSERT(other.isValid(), return false);
     if (m_hooks.intermediate)
         return m_hooks.intermediate(*this, other);
-    return !m_id.isEmpty() && m_id == other.m_id && m_name == other.m_name;
+    return m_id && m_id == other.m_id && m_name == other.m_name;
 }
 
 TestResult TestResult::intermediateResult() const

src/plugins/autotest/testresult.h
@@ -9,6 +9,8 @@
 
 #include <QColor>
 
+#include <optional>
+
 namespace Autotest {
 
 class ITestTreeItem;
@@ -76,14 +78,15 @@ class TestResult
 {
 public:
     TestResult() = default;
-    TestResult(const QString &id, const QString &name, const ResultHooks &hooks = {});
+    TestResult(const std::optional<QString> &id, const QString &name,
+               const ResultHooks &hooks = {});
     virtual ~TestResult() {}
 
     bool isValid() const;
     const QString outputString(bool selected) const;
     const ITestTreeItem *findTestTreeItem() const;
 
-    QString id() const { return m_id; }
+    QString id() const { return m_id.value_or(QString{}); }
     QString name() const { return m_name; }
     ResultType result() const { return m_result; }
     QString description() const { return m_description; }
@@ -106,7 +109,7 @@ public:
     TestResult intermediateResult() const;
 
 private:
-    QString m_id;
+    std::optional<QString> m_id;
     QString m_name;
     ResultType m_result = ResultType::Invalid;  // the real result..
     QString m_description;

src/plugins/autotest/testresultdelegate.cpp
@@ -52,8 +52,6 @@ void TestResultDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
 
     const LayoutPositions positions(opt, resultFilterModel);
     const TestResult testResult = resultFilterModel->testResult(index);
-    QTC_ASSERT(testResult.isValid(), painter->restore(); return);
-
     const QWidget *widget = dynamic_cast<const QWidget*>(painter->device());
     QWindow *window = widget ? widget->window()->windowHandle() : nullptr;
 
@@ -130,7 +128,6 @@ QSize TestResultDelegate::sizeHint(const QStyleOptionViewItem &option, const QMo
 
     if (selected) {
         const TestResult testResult = resultFilterModel->testResult(index);
-        QTC_ASSERT(testResult.isValid(), return {});
         QString output = testResult.outputString(selected);
         limitTextOutput(output);
         output.replace('\n', QChar::LineSeparator);

src/plugins/autotest/testresultmodel.cpp
@@ -91,7 +91,6 @@ QVariant TestResultItem::data(int column, int role) const
 
 void TestResultItem::updateDescription(const QString &description)
 {
-    QTC_ASSERT(m_testResult.isValid(), return);
     m_testResult.setDescription(description);
 }
 
@@ -241,7 +240,7 @@ void TestResultModel::addTestResult(const TestResult &testResult, bool autoExpan
         if (lastRow >= 0) {
             TestResultItem *current = rootItem()->childAt(lastRow);
             const TestResult result = current->testResult();
-            if (result.isValid() && result.result() == ResultType::MessageCurrentTest) {
+            if (!result.isValid() && result.result() == ResultType::MessageCurrentTest) {
                 current->updateDescription(testResult.description());
                 emit dataChanged(current->index(), current->index());
                 return;
@@ -290,7 +289,7 @@ void TestResultModel::addTestResult(const TestResult &testResult, bool autoExpan
         if (lastRow >= 0) {
             TestResultItem *current = rootItem()->childAt(lastRow);
             const TestResult result = current->testResult();
-            if (result.isValid() && result.result() == ResultType::MessageCurrentTest) {
+            if (!result.isValid() && result.result() == ResultType::MessageCurrentTest) {
                 rootItem()->insertChild(current->index().row(), newItem);
                 return;
             }

src/plugins/autotest/testresultspane.cpp
@@ -633,14 +633,11 @@ TestResult TestResultsPane::getTestResult(const QModelIndex &idx)
 {
     if (!idx.isValid())
         return {};
-    const TestResult result = m_filterModel->testResult(idx);
-    QTC_CHECK(result.isValid());
-    return result;
+    return m_filterModel->testResult(idx);
 }
 
 void TestResultsPane::onCopyItemTriggered(const TestResult &result)
 {
-    QTC_ASSERT(result.isValid(), return);
     setClipboardAndSelection(result.outputString(true));
 }
 
@@ -689,7 +686,8 @@ QString TestResultsPane::getWholeOutput(const QModelIndex &parent)
     for (int row = 0, count = m_model->rowCount(parent); row < count; ++row) {
         QModelIndex current = m_model->index(row, 0, parent);
         const TestResult result = m_model->testResult(current);
-        QTC_ASSERT(result.isValid(), continue);
+        if (!result.isValid())
+            continue;
         if (auto item = m_model->itemForIndex(current))
             output.append(item->resultString()).append('\t');
         output.append(result.outputString(true)).append('\n');
@@ -706,7 +704,6 @@ void TestResultsPane::createMarks(const QModelIndex &parent)
     for (int row = 0, count = m_model->rowCount(parent); row < count; ++row) {
         const QModelIndex index = m_model->index(row, 0, parent);
         const TestResult result = m_model->testResult(index);
-        QTC_ASSERT(result.isValid(), continue);
 
         if (m_model->hasChildren(index))
             createMarks(index);

src/plugins/autotest/testrunner.cpp
@@ -784,7 +784,7 @@ void TestRunner::onFinished()
 
 void TestRunner::reportResult(ResultType type, const QString &description)
 {
-    TestResult result("internal", {});
+    TestResult result(std::nullopt, {});
     result.setResult(type);
     result.setDescription(description);
     emit testResultReady(result);