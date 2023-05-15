96d9a4950eea8dab0708f9fb958d554de506f6cf
hjk <hjk@qt.io>
2023-05-15 15:15:17 +0200

    Haskell: More compact settings/manager setup
    Use PagedSettings, move related parts Manager interface there, too.
    
    Change-Id: I079dad8bbbea39d2424a25867b08c34ab0234ad5

--- src/plugins/haskell/haskellmanager.cpp
+++ src/plugins/haskell/haskellmanager.cpp
@@ -3,9 +3,11 @@
 
 #include "haskellmanager.h"
 
+#include "haskellsettings.h"
 #include "haskelltr.h"
 
 #include <coreplugin/messagemanager.h>
+
 #include <utils/algorithm.h>
 #include <utils/commandline.h>
 #include <utils/hostosinfo.h>
@@ -13,33 +15,12 @@
 #include <utils/process.h>
 #include <utils/processenums.h>
 
-#include <QCoreApplication>
 #include <QDir>
 #include <QFileInfo>
-#include <QSettings>
-
-#include <unordered_map>
-
-static const char kStackExecutableKey[] = "Haskell/StackExecutable";
 
 using namespace Utils;
 
-namespace Haskell {
-namespace Internal {
-
-class HaskellManagerPrivate
-{
-public:
-    FilePath stackExecutable;
-};
-
-Q_GLOBAL_STATIC(HaskellManagerPrivate, m_d)
-Q_GLOBAL_STATIC(HaskellManager, m_instance)
-
-HaskellManager *HaskellManager::instance()
-{
-    return m_instance;
-}
+namespace Haskell::Internal {
 
 FilePath HaskellManager::findProjectDirectory(const FilePath &filePath)
 {
@@ -66,19 +47,6 @@ FilePath defaultStackExecutable()
     return FilePath::fromString("stack");
 }
 
-FilePath HaskellManager::stackExecutable()
-{
-    return m_d->stackExecutable;
-}
-
-void HaskellManager::setStackExecutable(const FilePath &filePath)
-{
-    if (filePath == m_d->stackExecutable)
-        return;
-    m_d->stackExecutable = filePath;
-    emit m_instance->stackExecutableChanged(m_d->stackExecutable);
-}
-
 void HaskellManager::openGhci(const FilePath &haskellFile)
 {
     const QList<MimeType> mimeTypes = mimeTypesForFileName(haskellFile.toString());
@@ -89,25 +57,9 @@ void HaskellManager::openGhci(const FilePath &haskellFile)
                       + (isHaskell ? QStringList{haskellFile.fileName()} : QStringList());
     Process p;
     p.setTerminalMode(TerminalMode::Detached);
-    p.setCommand({stackExecutable(), args});
+    p.setCommand({settings().stackPath.filePath(), args});
     p.setWorkingDirectory(haskellFile.absolutePath());
     p.start();
 }
 
-void HaskellManager::readSettings(QSettings *settings)
-{
-    m_d->stackExecutable = FilePath::fromString(
-        settings->value(kStackExecutableKey, defaultStackExecutable().toString()).toString());
-    emit m_instance->stackExecutableChanged(m_d->stackExecutable);
-}
-
-void HaskellManager::writeSettings(QSettings *settings)
-{
-    if (m_d->stackExecutable == defaultStackExecutable())
-        settings->remove(kStackExecutableKey);
-    else
-        settings->setValue(kStackExecutableKey, m_d->stackExecutable.toString());
-}
-
-} // namespace Internal
-} // namespace Haskell
+} // Haskell::Internal
