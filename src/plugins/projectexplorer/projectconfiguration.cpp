75c389598111b5e083da8271dc3e0fca3ca78100
hjk <hjk@qt.io>
2023-05-22 14:15:09 +0200

    ProjectExplorer: Remove ProjectConfiguration::mapFromBuildDeviceToGlobalPath
    Instead, inline it in its only user ever. The functionality wasn't
    as widely needed as originally thought.
    
    Change-Id: I9422b37663a2a05d7f060e10fac16c2b03116959

--- src/plugins/projectexplorer/projectconfiguration.cpp
+++ src/plugins/projectexplorer/projectconfiguration.cpp
@@ -3,10 +3,8 @@
 
 #include "projectconfiguration.h"
 
-#include "kitinformation.h"
 #include "target.h"
 
-#include <projectexplorer/devicesupport/idevice.h>
 #include <utils/algorithm.h>
 #include <utils/qtcassert.h>
 
@@ -108,13 +106,6 @@ bool ProjectConfiguration::fromMap(const QVariantMap &map)
     m_displayName.fromMap(map, DISPLAY_NAME_KEY);
     AspectContainer::fromMap(map);
     return true;
-}
-
-FilePath ProjectConfiguration::mapFromBuildDeviceToGlobalPath(const FilePath &path) const
-{
-    IDevice::ConstPtr dev = BuildDeviceKitAspect::device(kit());
-    QTC_ASSERT(dev, return path);
-    return dev->filePath(path.path());
 }
 
 Id ProjectExplorer::idFromMap(const QVariantMap &map)
