eec0540b75630b28fe1d2155a6b96bc65ec045c6
hjk <hjk@qt.io>
2023-06-21 16:54:11 +0200

    ProjectExplorer: Merge DeviceProcessList into ProcessList
    The abstraction was not used anymore.
    
    Change-Id: I756ae65b9164d0032c3e6a26c3a123fc50f72a75

--- src/plugins/projectexplorer/devicesupport/processlist.cpp
+++ src/plugins/projectexplorer/devicesupport/processlist.cpp
@@ -3,7 +3,11 @@
 
 #include "processlist.h"
 
-#include <projectexplorer/devicesupport/idevice.h>
+#include "../projectexplorertr.h"
+
+#include <utils/processinfo.h>
+#include <utils/qtcassert.h>
+#include <utils/treemodel.h>
 #include <utils/processinfo.h>
 
 #include <QTimer>
@@ -17,15 +21,140 @@
 using namespace Utils;
 
 namespace ProjectExplorer {
+namespace Internal {
+
+enum State { Inactive, Listing, Killing };
+
+class DeviceProcessTreeItem : public TreeItem
+{
+public:
+    DeviceProcessTreeItem(const ProcessInfo &p, Qt::ItemFlags f) : process(p), fl(f) {}
+
+    QVariant data(int column, int role) const final;
+    Qt::ItemFlags flags(int) const final { return fl; }
+
+    ProcessInfo process;
+    Qt::ItemFlags fl;
+};
+
+class DeviceProcessListPrivate
+{
+public:
+    DeviceProcessListPrivate(const IDevice::ConstPtr &device)
+        : device(device)
+    { }
+
+    qint64 ownPid = -1;
+    const IDevice::ConstPtr device;
+    State state = Inactive;
+    TreeModel<TypedTreeItem<DeviceProcessTreeItem>, DeviceProcessTreeItem> model;
+};
+
+} // namespace Internal
+
+using namespace Internal;
 
 ProcessList::ProcessList(const IDevice::ConstPtr &device, QObject *parent)
-    : DeviceProcessList(device, parent)
+    : QObject(parent), d(std::make_unique<DeviceProcessListPrivate>(device))
 {
+    d->model.setHeader({Tr::tr("Process ID"), Tr::tr("Command Line")});
+
 #if defined(Q_OS_UNIX)
     setOwnPid(getpid());
 #elif defined(Q_OS_WIN)
     setOwnPid(GetCurrentProcessId());
 #endif
+}
+
+ProcessList::~ProcessList() = default;
+
+void ProcessList::update()
+{
+    QTC_ASSERT(d->state == Inactive, return);
+    QTC_ASSERT(device(), return);
+
+    d->model.clear();
+    d->model.rootItem()->appendChild(
+                new DeviceProcessTreeItem(
+                    {0, Tr::tr("Fetching process list. This might take a while."), ""},
+                    Qt::NoItemFlags));
+    d->state = Listing;
+    doUpdate();
+}
+
+void ProcessList::reportProcessListUpdated(const QList<ProcessInfo> &processes)
+{
+    QTC_ASSERT(d->state == Listing, return);
+    setFinished();
+    d->model.clear();
+    for (const ProcessInfo &process : processes) {
+        Qt::ItemFlags fl;
+        if (process.processId != d->ownPid)
+            fl = Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+        d->model.rootItem()->appendChild(new DeviceProcessTreeItem(process, fl));
+    }
+
+    emit processListUpdated();
+}
+
+void ProcessList::killProcess(int row)
+{
+    QTC_ASSERT(row >= 0 && row < d->model.rootItem()->childCount(), return);
+    QTC_ASSERT(d->state == Inactive, return);
+    QTC_ASSERT(device(), return);
+
+    d->state = Killing;
+    doKillProcess(at(row));
+}
+
+void ProcessList::setOwnPid(qint64 pid)
+{
+    d->ownPid = pid;
+}
+
+void ProcessList::reportProcessKilled()
+{
+    QTC_ASSERT(d->state == Killing, return);
+    setFinished();
+    emit processKilled();
+}
+
+ProcessInfo ProcessList::at(int row) const
+{
+    return d->model.rootItem()->childAt(row)->process;
+}
+
+QAbstractItemModel *ProcessList::model() const
+{
+    return &d->model;
+}
+
+QVariant DeviceProcessTreeItem::data(int column, int role) const
+{
+    if (role == Qt::DisplayRole || role == Qt::ToolTipRole) {
+        if (column == 0)
+            return process.processId ? process.processId : QVariant();
+        else
+            return process.commandLine;
+    }
+    return QVariant();
+}
+
+void ProcessList::setFinished()
+{
+    d->state = Inactive;
+}
+
+IDevice::ConstPtr ProcessList::device() const
+{
+    return d->device;
+}
+
+void ProcessList::reportError(const QString &message)
+{
+    QTC_ASSERT(d->state != Inactive, return);
+    setFinished();
+    emit error(message);
 }
 
 void ProcessList::doKillProcess(const ProcessInfo &processInfo)
@@ -40,7 +169,7 @@ void ProcessList::doKillProcess(const ProcessInfo &processInfo)
 
 void ProcessList::handleUpdate()
 {
-    reportProcessListUpdated(ProcessInfo::processInfoList(DeviceProcessList::device()->rootPath()));
+    reportProcessListUpdated(ProcessInfo::processInfoList(ProcessList::device()->rootPath()));
 }
 
 void ProcessList::doUpdate()
@@ -58,4 +187,4 @@ void ProcessList::reportDelayedKillStatus(const QString &errorMessage)
     m_signalOperation.reset();
 }
 
-} // namespace ProjectExplorer
+} // ProjectExplorer
--- src/plugins/projectexplorer/devicesupport/processlist.h
+++ src/plugins/projectexplorer/devicesupport/processlist.h
@@ -3,28 +3,59 @@
 
 #pragma once
 
-#include "deviceprocesslist.h"
+#include "../projectexplorer_export.h"
 #include "idevice.h"
+
+#include <QAbstractItemModel>
+#include <QList>
+
+#include <memory>
+
+namespace Utils { class ProcessInfo; }
 
 namespace ProjectExplorer {
 
-class PROJECTEXPLORER_EXPORT ProcessList : public DeviceProcessList
+namespace Internal { class DeviceProcessListPrivate; }
+
+class PROJECTEXPLORER_EXPORT ProcessList : public QObject
 {
     Q_OBJECT
 
 public:
-    explicit ProcessList(const IDeviceConstPtr &device, QObject *parent = nullptr);
+    ProcessList(const IDeviceConstPtr &device, QObject *parent = nullptr);
+    ~ProcessList() override;
+
+    void update();
+    void killProcess(int row);
+    void setOwnPid(qint64 pid);
+
+    Utils::ProcessInfo at(int row) const;
+    QAbstractItemModel *model() const;
+
+signals:
+    void processListUpdated();
+    void error(const QString &errorMsg);
+    void processKilled();
+
+protected:
+    void reportError(const QString &message);
+    void reportProcessKilled();
+    void reportProcessListUpdated(const QList<Utils::ProcessInfo> &processes);
+
+    IDeviceConstPtr device() const;
 
 private:
-    void doUpdate() override;
-    void doKillProcess(const Utils::ProcessInfo &process) override;
+    void doUpdate();
+    void doKillProcess(const Utils::ProcessInfo &process);
 
-private:
     void handleUpdate();
     void reportDelayedKillStatus(const QString &errorMessage);
 
-private:
     DeviceProcessSignalOperation::Ptr m_signalOperation;
+
+    void setFinished();
+
+    const std::unique_ptr<Internal::DeviceProcessListPrivate> d;
 };
 
 } // namespace ProjectExplorer
