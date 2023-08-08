97fcec85c49687509543e7cacb895ea71e1a46eb
Jarek Kobus <jaroslaw.kobus@qt.io>
2023-08-08 20:50:00 +0200

    Valgrind test: Use Utils::Process instead of QProcess
    Change-Id: Id7a0611fd7d36adc22993f50cce87cd30c6a336c

--- src/plugins/valgrind/valgrindmemcheckparsertest.h
+++ src/plugins/valgrind/valgrindmemcheckparsertest.h
@@ -3,12 +3,13 @@
 
 #pragma once
 
+#include <utils/process.h>
+
 #include <QObject>
 #include <QStringList>
 #include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
-class QProcess;
 class QTcpServer;
 QT_END_NAMESPACE
 
@@ -40,7 +41,7 @@ private:
     void initTest(const QString &testfile, const QStringList &otherArgs = {});
 
     QTcpServer *m_server = nullptr;
-    QProcess *m_process = nullptr;
+    std::unique_ptr<Utils::Process> m_process;
     std::unique_ptr<QTcpSocket> m_socket;
 };
 
