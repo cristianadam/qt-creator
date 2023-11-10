a0b32cf97577e9fd5d0bd98836f82301afd05f83
Thomas Hartmann <thomas.hartmann@qt.io>
2023-11-10 10:36:40 +0100

    WIP: enable most plugins and options to test
    Change-Id: I96305578c658e058faec8993dfd214fd570e0521

--- dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
+++ dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
@@ -1,13 +1,13 @@
-set(IDE_VERSION "4.4.0")                              # The IDE version.
-set(IDE_VERSION_COMPAT "4.4.0")                       # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "4.4.0")                      # The IDE display version.
-set(IDE_COPYRIGHT_YEAR "2023")                        # The IDE current copyright year.
+set(IDE_VERSION "0.0.1")                              # The IDE version.
+set(IDE_VERSION_COMPAT "0.0.1")                       # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "0.0.1")                      # The IDE display version.
+set(IDE_COPYRIGHT_YEAR "2024")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
 set(IDE_COPY_SETTINGSVARIANT "Nokia")                 # The IDE settings to initially import.
-set(IDE_DISPLAY_NAME "Qt Design Studio")              # The IDE display name.
-set(IDE_ID "qtdesignstudio")                          # The IDE id (no spaces, lowercase!)
-set(IDE_CASED_ID "QtDesignStudio")                    # The cased IDE id (no spaces!)
+set(IDE_DISPLAY_NAME "Qt Greenhouse")              # The IDE display name.
+set(IDE_ID "qtgreenhouse")                          # The IDE id (no spaces, lowercase!)
+set(IDE_CASED_ID "QtGreenHouse")                    # The cased IDE id (no spaces!)
 set(IDE_BUNDLE_IDENTIFIER "org.qt-project.${IDE_ID}") # The macOS application bundle identifier.
 
 set(PROJECT_USER_FILE_EXTENSION .qtds)
