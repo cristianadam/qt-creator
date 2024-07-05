f39223599d39ff0f3b643a4aec28b6f9bebacb40
hjk <hjk@qt.io>
2024-07-05 11:37:24 +0200

    Core: Prepare de-virtualization of IContext::contextHelp
    ... by providing a way to specify modified implementation
    by setting a functor.
    
    This is somewhat complicated by the ususual tail-recursive
    iteration using continuation, which might or might not be
    open for simplifications later.
    
    Change-Id: Ib0907dd00fdb2ef15242a7ec2e25a5f8a9cc6979

--- src/plugins/coreplugin/icontext.cpp
+++ src/plugins/coreplugin/icontext.cpp
@@ -9,6 +9,25 @@
 
 namespace Core {
 
+void IContext::contextHelp(const HelpCallback &callback) const
+{
+    if (m_contextHelpProvider) {
+        m_contextHelpProvider(callback);
+    } else {
+        // This is important as this triggers the continued iteration
+        // through other contexts that may provide items.
+        // FIXME: This could be simpler.
+        callback({});
+    }
+}
+
+void IContext::setContextHelp(const HelpItem &item)
+{
+    m_contextHelpProvider = [item](const HelpCallback &callback) {
+        callback(item);
+    };
+}
+
 void IContext::attach(QWidget *widget, const Context &context, const HelpItem &help)
 {
     auto icontext = new IContext(widget); // As QObject parent.
