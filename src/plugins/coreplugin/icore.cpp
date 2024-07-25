3e8321ef509d94e10ccf36830222c049126d184d
Marcus Tillmanns <marcus.tillmanns@qt.io>
2024-07-25 10:28:39 +0200

    Core: Use AppInfo.Paths
    Change-Id: Ic4aff7e2b8957ab125560eefe1cff4d9df5ffd0a

--- src/plugins/coreplugin/icore.cpp
+++ src/plugins/coreplugin/icore.cpp
@@ -618,9 +618,7 @@ static QString pathHelper(const QString &rel)
 */
 FilePath ICore::resourcePath(const QString &rel)
 {
-    return FilePath::fromString(
-               QDir::cleanPath(QCoreApplication::applicationDirPath() + '/' + RELATIVE_DATA_PATH))
-           / rel;
+    return appInfo().resources / rel;
 }
 
 /*!
@@ -635,17 +633,7 @@ FilePath ICore::resourcePath(const QString &rel)
 
 FilePath ICore::userResourcePath(const QString &rel)
 {
-    // Create qtcreator dir if it doesn't yet exist
-    const QString configDir = QFileInfo(settings(QSettings::UserScope)->fileName()).path();
-    const QString urp = configDir + '/' + appInfo().id;
-
-    if (!QFileInfo::exists(urp + QLatin1Char('/'))) {
-        QDir dir;
-        if (!dir.mkpath(urp))
-            qWarning() << "could not create" << urp;
-    }
-
-    return FilePath::fromString(urp + pathHelper(rel));
+    return appInfo().userResources / rel;
 }
 
 /*!
