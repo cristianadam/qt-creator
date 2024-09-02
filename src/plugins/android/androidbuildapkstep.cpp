13e7abec99da1c4cf9fcb1476b3aee6618a982fa
Andrii Semkiv <andrii.semkiv@qt.io>
2024-09-02 17:16:12 +0200

    Android: Replace occurrences of FilePath::toString in androidbuildapkstep.cpp
    Change-Id: I64d7e148ce58668174400856a946742acdfaefe3

--- src/plugins/android/androidbuildapkstep.cpp
+++ src/plugins/android/androidbuildapkstep.cpp
@@ -413,7 +413,7 @@ bool AndroidBuildApkWidget::isOpenSslLibsIncluded()
 
 QString AndroidBuildApkWidget::openSslIncludeFileContent(const FilePath &projectPath)
 {
-    QString openSslPath = AndroidConfig::openSslLocation().toString();
+    QString openSslPath = AndroidConfig::openSslLocation().path();
     if (projectPath.endsWith(".pro"))
         return "android: include(" + openSslPath + "/openssl.pri)";
     if (projectPath.endsWith("CMakeLists.txt"))
@@ -747,7 +747,7 @@ Tasking::GroupItem AndroidBuildApkStep::runRecipe()
         QString applicationBinary;
         if (!version->supportsMultipleQtAbis()) {
             QTC_ASSERT(androidAbis.size() == 1, return false);
-            applicationBinary = buildSystem()->buildTarget(buildKey).targetFilePath.toString();
+            applicationBinary = buildSystem()->buildTarget(buildKey).targetFilePath.path();
             FilePath androidLibsDir = androidBuildDir / "libs" / androidAbis.first();
             for (const FilePath &target : targets) {
                 if (!copyFileIfNewer(target, androidLibsDir.pathAppended(target.fileName()))) {
@@ -804,10 +804,10 @@ Tasking::GroupItem AndroidBuildApkStep::runRecipe()
 
         QString qmlRootPath = bs->extraData(buildKey, "QML_ROOT_PATH").toString();
         if (qmlRootPath.isEmpty())
-            qmlRootPath = target()->project()->rootProjectDirectory().toString();
+            qmlRootPath = target()->project()->rootProjectDirectory().path();
         deploySettings["qml-root-path"] = qmlRootPath;
 
-        QFile f{m_inputFile.toString()};
+        QFile f{m_inputFile.path()};
         if (!f.open(QIODevice::WriteOnly)) {
             reportWarningOrError(Tr::tr("Cannot open androiddeployqt input file \"%1\" for writing.")
                                      .arg(m_inputFile.toUserOutput()), Task::Error);
