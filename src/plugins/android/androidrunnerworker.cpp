ce02fa63d6472e8b66613ffe527a10458ec47114
Andrii Semkiv <andrii.semkiv@qt.io>
2024-09-03 13:09:18 +0200

    Android: Replace occurrences of FilePath::toString in androidrunnerworker.cpp
    Change-Id: Ic67e197eae8f3a7ba90a69952694079bb337c227

--- src/plugins/android/androidrunnerworker.cpp
+++ src/plugins/android/androidrunnerworker.cpp
@@ -659,8 +659,8 @@ static ExecutableItem uploadDebugServerRecipe(RunnerStorage *storage, const QStr
     };
 
     const auto onServerUploadSetup = [storage, tempDebugServerPathStorage](Process &process) {
-        process.setCommand(
-            storage->adbCommand({"push", storage->m_debugServerPath.toString(), *tempDebugServerPathStorage}));
+        process.setCommand(storage->adbCommand(
+            {"push", storage->m_debugServerPath.nativePath(), *tempDebugServerPathStorage}));
     };
 
     const auto onServerCopySetup = [storage, tempDebugServerPathStorage, debugServerFileName](Process &process) {
