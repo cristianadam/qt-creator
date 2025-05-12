5224e75805ba61e5308ddf0280635bb55ba99b1f
Marcus Tillmanns <marcus.tillmanns@qt.io>
2025-05-12 09:54:35 +0200

    Utils: Fix compiling with gcc 10
    std::views is not fully supported on gcc 10.
    (see https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2021/p2415r2.html)
    
    Switch to using Utils::transform instead.
    
    Change-Id: I120a44bb084b674c8bf75257a20e13311091484f

--- tests/manual/widgets/layoutbuilder/tst_manual_widgets_layoutbuilder.cpp
+++ tests/manual/widgets/layoutbuilder/tst_manual_widgets_layoutbuilder.cpp
@@ -165,7 +165,7 @@ int main(int argc, char *argv[])
             new ManualTest::ThemeSelector,
             Label { text("QtcButton:") },
             Flow {
-                std::views::transform(Utils::ranges::MetaEnum<Utils::QtcButton::Role>(), [](int r) {
+                Utils::transform<QList>(Utils::ranges::MetaEnum<Utils::QtcButton::Role>(), [](int r) {
                     return Button{
                         text(QMetaEnum::fromType<Utils::QtcButton::Role>().valueToKey(r)),
                         role((Utils::QtcButton::Role) r)
@@ -174,7 +174,7 @@ int main(int argc, char *argv[])
             },
             Label { text("QtcButton with Icons:") },
             Flow {
-                std::views::transform(Utils::ranges::MetaEnum<Utils::QtcButton::Role>(), [](int r) {
+                Utils::transform<QList>(Utils::ranges::MetaEnum<Utils::QtcButton::Role>(), [](int r) {
                     return Button{
                         text(QMetaEnum::fromType<Utils::QtcButton::Role>().valueToKey(r)),
                         role((Utils::QtcButton::Role) r),
