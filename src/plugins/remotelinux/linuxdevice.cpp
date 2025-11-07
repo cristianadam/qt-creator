df18e85aa7e8d37b0db65a757bdeaaad0f10f080
hjk <hjk@qt.io>
2025-11-07 15:32:49 +0100

    Utils: Remove UnavailableFileAccess
    Interpret a nullptr as unavailable.
    
    Change-Id: I11e28ac43a4da0d74ea4143e74b0492ff1d30338

--- src/plugins/remotelinux/linuxdevice.cpp
+++ src/plugins/remotelinux/linuxdevice.cpp
@@ -406,10 +406,7 @@ class LinuxDevicePrivate
 public:
     explicit LinuxDevicePrivate(LinuxDevice *parent)
         : q(parent)
-        , m_disconnectedAccess(std::make_shared<UnavailableDeviceFileAccess>())
-    {
-        q->setFileAccess(m_disconnectedAccess);
-    }
+    {}
 
     void setOsType(OsType osType)
     {
@@ -439,14 +436,13 @@ public:
     {
         QMutexLocker locker(&m_shellMutex);
         DeviceManager::setDeviceState(q->id(), IDevice::DeviceDisconnected, announce);
-        q->setFileAccess(m_disconnectedAccess);
+        q->setFileAccess(nullptr);
         m_cmdBridgeAccess.reset();
         m_scriptAccess.reset();
     }
 
     LinuxDevice *q = nullptr;
 
-    std::shared_ptr<UnavailableDeviceFileAccess> m_disconnectedAccess;
     std::shared_ptr<LinuxDeviceAccess> m_scriptAccess;
     std::shared_ptr<CmdBridge::FileAccess> m_cmdBridgeAccess;
 
@@ -1380,7 +1376,7 @@ void LinuxDevicePrivate::setupShellPhase2(const Result<> &result,
         }
     } else {
         DEBUG("Failed to setup state");
-        q->setFileAccess(m_disconnectedAccess);
+        q->setFileAccess(nullptr);
         q->setDeviceState(IDevice::DeviceDisconnected);
     }
 
