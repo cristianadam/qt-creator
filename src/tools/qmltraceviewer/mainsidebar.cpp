23368424ef12e55deb12e78a3b504ab30e10846c
Marcus Tillmanns <marcus.tillmanns@qt.io>
2026-06-11 10:27:27 +0200

    Profiler: Add main sidebar
    Change-Id: If7dbf7d9448b54e73d279a7ddac7194b9f14c77c

---
+++ src/tools/qmltraceviewer/mainsidebar.cpp
@@ -0,0 +1,72 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include "mainsidebar.h"
+
+#include <qmlprofiler/profilertr.h>
+
+#include <utils/filepath.h>
+#include <utils/stylehelper.h>
+
+#include <QLabel>
+#include <QListWidget>
+#include <QVBoxLayout>
+
+using namespace Profiler;
+using namespace Utils;
+using namespace Utils::StyleHelper;
+
+namespace QmlTraceViewer {
+
+MainSidebar::MainSidebar(QWidget *parent)
+    : QWidget(parent)
+{
+    auto title = new QLabel(Tr::tr("Traces"));
+    title->setContentsMargins(SpacingTokens::PaddingHM, SpacingTokens::PaddingVS,
+                              SpacingTokens::PaddingHM, SpacingTokens::PaddingVS);
+
+    m_list = new QListWidget;
+    m_list->setFrameShape(QFrame::NoFrame);
+
+    auto layout = new QVBoxLayout(this);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->addWidget(title);
+    layout->addWidget(m_list);
+
+    connect(m_list, &QListWidget::currentItemChanged, this, [this](QListWidgetItem *current) {
+        if (current)
+            emit traceActivated(FilePath::fromVariant(current->data(Qt::UserRole)));
+    });
+}
+
+void MainSidebar::addTrace(const FilePath &filePath)
+{
+    const QVariant data = filePath.toVariant();
+
+    // Programmatic selection must not trigger a reload, so block currentItemChanged.
+    const QSignalBlocker blocker(m_list);
+
+    for (int i = 0; i < m_list->count(); ++i) {
+        QListWidgetItem *item = m_list->item(i);
+        if (item->data(Qt::UserRole) == data) {
+            m_list->setCurrentItem(item);
+            return;
+        }
+    }
+
+    auto item = new QListWidgetItem(filePath.fileName());
+    item->setToolTip(filePath.toUserOutput());
+    item->setData(Qt::UserRole, data);
+    m_list->addItem(item);
+    m_list->setCurrentItem(item);
+}
+
+FilePath MainSidebar::currentTrace() const
+{
+    if (QListWidgetItem *item = m_list->currentItem())
+        return FilePath::fromVariant(item->data(Qt::UserRole));
+    return {};
+}
+
+} // namespace QmlTraceViewer
